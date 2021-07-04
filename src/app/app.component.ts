7280bc9d480e4e198bb839d120a17631a84427a3
Captain Storm <CaptainStorm21@users.noreply.github.com>
2021-07-04 13:44:11 -0700

    title change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular Module Route System Part 2' ;
+  title = 'Angular Module Route System Part 3' ;
 }
